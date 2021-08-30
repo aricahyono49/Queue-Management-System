af37ed28ff0c389ddd10fe2024209edd2c356fdb
Ari Cahyono <cahyono.ari80@gmail.com>
2021-08-30 23:17:39 +0700

    feat : add side-navigasi to all dashboard pages

---
+++ src/pages/dashboard/Beranda/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { SideNavigasi } from '../../../components/SideNavigasi'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export const Beranda = () => {
+    return (
+      <div className="row">
+        <div className="col-4">
+          <SideNavigasi position="beranda"/>
+        </div>
+        <div className="col-6">
+            components BERANDA taruh sini yah
+        </div>
+      </div>
+    );
+}
---
+++ src/pages/dashboard/BookNomorAntrian/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { SideNavigasi } from "../../../components/SideNavigasi";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export const BookNomorAntrian = () => {
+  return (
+    <div className="row">
+      <div className="col-4">
+        <SideNavigasi position="book nomor antrian" />
+      </div>
+      <div className="col-6">components BOOK NOMOR ANTRIAN taruh sini yah</div>
+    </div>
+  );
+};
