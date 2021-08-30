7052c069b8f515c49e4d8305896e475703b8afaf
Ari Cahyono <cahyono.ari80@gmail.com>
2021-08-30 23:18:34 +0700

    feat : add all pages in main index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Beranda } from './pages/dashboard/Beranda';
+import { BookNomorAntrian } from './pages/dashboard/BookNomorAntrian';
+import { DaftarBank } from './pages/dashboard/DaftarBank';
+import { DetailInfoAntrian } from './pages/dashboard/DetailInfoAntrian';
+import { Register } from './pages/auth/Register';
+import { Login } from './pages/auth/Login';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="col-9 offset-2">
+      <Login/>
+      <Register/>
+      <Beranda />
+      <BookNomorAntrian />
+      <DaftarBank />
+      <DetailInfoAntrian />
+    </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
